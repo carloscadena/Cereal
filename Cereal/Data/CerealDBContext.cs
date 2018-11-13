8a0f0578e069c810adc2da04357a2b94f49dbc76
carlos cadena <carloscadena@live.com>
2018-11-13 12:37:41 -0800

    Allisa and I pair programmed on setting all of this up

---
+++ Cereal/Data/CerealDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cereal.Data
+{
+    public class CerealDBContext : DbContext
+    {
+        public CerealDBContext(DbContextOptions<CerealDBContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- Cereal/Startup.cs
+++ Cereal/Startup.cs
@@ -5,16 +5,33 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Cereal.Data;
 
 namespace Cereal
 {
     public class Startup
     {
+        private IConfiguration Configuration { get; set; }
+
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder().AddEnvironmentVariables();
+            builder.AddUserSecrets<Startup>();
+            Configuration = builder.Build();
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
+            services.AddDbContext<CerealDBContext>(options =>
+            options.UseSqlServer(Configuration["ConnectionStrings:ProductionDb"])
+            );
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,9 +42,13 @@ namespace Cereal
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                name: "default",
+                template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
