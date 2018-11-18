5226b7baaf51496ee40c9a813c47a214fd99ceaf
Allisa LeBeuf <ajelebeuf@gmail.com>
2018-11-18 13:39:24 -0800

    added authorization handler logic for employee email

---
+++ Cereal/Models/Handlers/EmployeeEmailRequirement.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Cereal.Models.Handlers
+{
+    public class EmployeeEmailRequirement : AuthorizationHandler<EmployeeEmailRequirement>, IAuthorizationRequirement
+    {   
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, EmployeeEmailRequirement requirement)
+        {
+            if (!context.User.HasClaim(email => email.Type == ClaimTypes.Email))
+            {
+                return Task.CompletedTask;
+            }
+
+            var employeeEmail = context.User.FindFirst(email => email.Type == ClaimTypes.Email).Value;
+
+            if (employeeEmail.Contains("@live.com"))
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
