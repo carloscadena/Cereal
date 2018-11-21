1f029a236142b4279548dbd9a6958e0bafe434c8
Allisa LeBeuf <al5906@gmail.com>
2018-11-20 19:58:13 -0800

    created view component

---
+++ Cereal/Components/Basket.cs
@@ -0,0 +1,21 @@
+﻿using Cereal.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cereal.Components
+{
+    public class Basket : ViewComponent
+    {
+        private CerealDBContext _context;
+
+        public Basket(CerealDBContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
