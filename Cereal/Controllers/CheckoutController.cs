6362c875680e163881f809c258d613db244e5e9a
Allisa LeBeuf <al5906@gmail.com>
2018-11-27 21:18:14 -0800

    added db logic to checkout controller

--- Cereal/Controllers/CheckoutController.cs
+++ Cereal/Controllers/CheckoutController.cs
@@ -2,15 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Cereal.Data;
+using Cereal.Models;
+using Cereal.Models.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Cereal.Controllers
 {
     public class CheckoutController : Controller
     {
-        public IActionResult Index()
+        private readonly IBasketItems _basket;
+        private readonly IProduct _product;
+        private UserManager<ApplicationUser> _userManager;
+        private CerealDBContext _context;
+
+        public CheckoutController(IProduct product, IBasketItems basket, UserManager<ApplicationUser> userManager, CerealDBContext context)
         {
-            return RedirectToAction("Index", "Receipt");
+            _basket = basket;
+            _product = product;
+            _userManager = userManager;
+            _context = context;
+
+        }
+
+        public async Task<IActionResult> Receipt()
+        {
+        var user = await _userManager.GetUserAsync(HttpContext.User);
+        var ID = user.Id;
+        var receipt = await _context.BasketItems.Where(x => x.UserID == ID).Include(product => product.Product).ToListAsync();
+           
+            return View(receipt);
         }
     }
 }
